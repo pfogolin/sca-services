cf9deba5799929e4ed393e0e43b296e55e890777
Pedro Henrique Iyda Fogolin <pedro.fogolin@grupopan.com>
2017-09-15 15:44:59 -0300

    implementando logica de bolsa de estudos do enem

--- microservices/aluno/controllers/mecController.js
+++ microservices/aluno/controllers/mecController.js
@@ -1,9 +1,9 @@
 module.exports.get = (req, res) => {    
     var arrBolsaEstudos = [0, 25, 50, 75, 100]
     var bolsaEstudos = arrBolsaEstudos[Math.floor(Math.random() * arrBolsaEstudos.length)]
-    var ret = {
-        "bolsaEnem" : bolsaEstudos
-    }
-    console.log(bolsaEstudos)
-    res.send(JSON.stringify(ret))
+    // var ret = {
+    //     "bolsaEnem" : bolsaEstudos
+    // }
+    //console.log(bolsaEstudos)
+    res.send(JSON.stringify(bolsaEstudos))
 }
